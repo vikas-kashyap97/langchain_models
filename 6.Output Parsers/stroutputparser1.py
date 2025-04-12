a4807bc2466a0040349ac43c5263b7b6db924a14
Vikas Kashyap <vikaskashyaprock@gmail.com>
2025-04-12 15:30:43 +0530

    commit

--- 6.Output Parsers/stroutputparser1.py
+++ 6.Output Parsers/stroutputparser1.py
@@ -1,6 +1,8 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
 
 load_dotenv()
 
@@ -23,3 +25,9 @@ template2 = PromptTemplate(
     input_variables=['topic']
     )
 
+parser = StrOutputParser()
+
+chain = template1 | model | parser | template2 | model | parser
+
+result = chain.invoke({'topic' : 'black hole'})
+print(result)
